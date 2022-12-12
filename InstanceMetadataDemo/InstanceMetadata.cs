cb542933ed5bc20af7240eb20fa933ca166c70f6
sywatchdan <danieljelwell@gmail.com>
2022-12-12 20:53:57 +0000

    Add project files.

---
+++ InstanceMetadataDemo/InstanceMetadata.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Amazon.EC2;
+using Amazon.EC2.Internal;
+using Amazon.EC2.Model;
+using Amazon.Util;
+using Amazon.Runtime.Internal.Transform;
+
+namespace InstanceMetadataDemo {
+    internal class InstanceMetadata {
+        public InstanceMetadata() { }
+
+        public string InspectMetadataItems(string startingPath) {
+
+            Dictionary<string, object> values = new Dictionary<string, object>();
+
+            // Get the name for the first object. This will either be the requested key, or 'Metadata' if no key was specified
+            string startingPathName = (startingPath == "/" ? "Metadata" : startingPath.Substring(startingPath.LastIndexOf("/") + 1));
+
+            // AWS SDK BUG FIX 1: Requested path needs to end with a trailing slash
+            if (startingPath.EndsWith("/") == false) { startingPath += "/"; }
+
+            // AWS SDK BUG FIX 2: Calling GetItems on a key value pair incorrectly returns the value as a child key. To get around this, we check
+            // that the value is equal to what it thinks is a child key, and that querying that child key returns null.
+            IEnumerable<string> metadataItems = EC2InstanceMetadata.GetItems(startingPath);
+
+            if (metadataItems.Count() == 1 && metadataItems.First() == EC2InstanceMetadata.GetData(startingPath) && EC2InstanceMetadata.GetData(startingPath + metadataItems.First()) is null) {
+                // This is just a key value pair. Add it to our dictionary
+                values.Add(startingPathName, EC2InstanceMetadata.GetData(startingPath));
+
+            } else {
+
+                // Not affected by the bug. Recurse through child keys
+                values.Add(startingPathName, RecurseItems(startingPath));
+            }
+
+            return (JsonConvert.SerializeObject(values));
+        }
+
+        private Dictionary<string, object> RecurseItems(string path) {
+
+            Dictionary<string, object> values = new Dictionary<string, object>();
+            
+            // Get all items (properties) in this key and loop through them
+            IEnumerable<string> metadataItems = EC2InstanceMetadata.GetItems(path);
+ 
+            foreach (string propertyName in metadataItems) {
+
+                // If the property name ends with a trailing slash, it has children to recurse through, otherwise it contains just a single value
+                if(propertyName.EndsWith("/")) {
+                    values.Add(propertyName.Remove(propertyName.Length - 1), RecurseItems(path + propertyName));
+
+                } else {
+                    string propertyData = EC2InstanceMetadata.GetData(path + propertyName);
+                    values.Add(propertyName, propertyData);
+                }
+            }
+            return values;
+        }
+        
+    }
+}
---
+++ InstanceMetadataDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Amazon.EC2;
+using Amazon.EC2.Internal;
+using Amazon.EC2.Model;
+using Amazon.Util;
+using System.Reflection;
+using Newtonsoft.Json;
+using InstanceMetadataDemo;
+
+Console.WriteLine("InstanceMetadataDemo");
+Console.WriteLine("This demo app will enumerate items in the EC2 Instance Metadata and output them as JSON");
+Console.WriteLine();
+Console.Write("Enter a specific value to search for or leave blank for all: ");
+
+string startingPath = "/" + Console.ReadLine();
+
+// Spin up a new InstanceMetadata class and get it to produce the output
+InstanceMetadata instanceMetadata = new InstanceMetadata();
+string outputJson = instanceMetadata.InspectMetadataItems(startingPath);
+
+// Print the response
+Console.WriteLine(outputJson);
+
